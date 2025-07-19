f0c7e1a849b9c1665ea9f6c55444e0a4e95d9c45
withered <sergeyzezyanov1@gmail.com>
2025-07-19 15:34:27 +0700

    Add pydantic model Activity level validation

--- api/schemas/activity_level.py
+++ api/schemas/activity_level.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class ActivityLevel(BaseModel):
-    """Activity level model"""
+    """
+    Activity level model.
+    Used to define different activity levels of users.
+    """
+    id: int = Field(frozen=True)
     description: str
     name: str
-    level: int
+    level: int = Field(ge=0)
 
     model_config = ConfigDict(from_attributes=True)
