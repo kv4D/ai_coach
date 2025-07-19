a8c3d7ceb979aaabb54850072518ee7b5d7ea7eb
withered <sergeyzezyanov1@gmail.com>
2025-07-19 17:04:28 +0700

    Add TrainingPlan pydantic model validation
    It checks if every day of the week is present

--- api/schemas/training_plan.py
+++ api/schemas/training_plan.py
@@ -12,3 +12,21 @@ class TrainingPlan(BaseModel):
     plan_description: str
 
     model_config = ConfigDict(from_attributes=True)
+
+    @field_validator('plan_description')
+    @classmethod
+    def check_plan_description(cls, text: str) -> str:
+        days = [
+            'понедельник',
+            'вторник',
+            'среда',
+            'четверг',
+            'пятница',
+            'суббота',
+            'воскресенье'
+        ]
+        
+        missing = [day for day in days if day not in text.lower()]
+        if missing:
+            raise ValueError(f"Some days are not in the plan: {', '.join(missing)}")
+        return text
