bcb4e419e034292f1500699132cb441ae4321e16
withered <sergeyzezyanov1@gmail.com>
2025-07-22 14:46:16 +0700

    Fix config load

--- bot/config.py
+++ bot/config.py
@@ -4,13 +4,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"),
+    model_config = SettingsConfigDict(env_file=".env",
                                       env_file_encoding='utf-8',
                                       extra='ignore')
-    
     DB_HOST: str
     TG_BOT_TOKEN: str
 
 
 # import this to use config
+print(os.path.join(os.path.dirname('api'), ".env"))
 config = Config() # type: ignore
