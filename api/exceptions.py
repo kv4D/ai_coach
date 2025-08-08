4abcd93aff061b03d909a9541a0d3fcf64892381
withered <sergeyzezyanov1@gmail.com>
2025-08-09 02:29:01 +0700

    Change exception handling registration
    Exception handling was implemented with decorator,
    now you need to import already prepared dictionary
    with handlers and their exceptions from exceptions.py

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,5 +1,6 @@
 """Custom exceptions and their status codes for the API."""
-
+from fastapi.responses import JSONResponse
+from fastapi import Request
 
 class BaseCustomException(Exception):
     """Base exception for all API errors."""
@@ -31,3 +32,25 @@ class UnexpectedError(BaseCustomException):
     """
     def __init__(self, message: str = "Unexpected error"):
         super().__init__(message, status_code=500)
+
+
+async def custom_error_handler(request: Request,
+                               exc: BaseCustomException) -> JSONResponse:
+    """
+    Handle all custom exceptions.
+    
+    All defined exceptions in `exceptions.py` will be handled here.
+    """
+    return JSONResponse(
+        status_code=exc.status,
+        content={
+            "message": exc.message,
+            "type": exc.__class__.__name__,  
+            "detail": str(exc)
+            }
+        )
+
+# import this to use for exception handling
+exception_handlers = {
+    BaseCustomException: custom_error_handler
+}
--- api/main.py
+++ api/main.py
@@ -1,36 +1,15 @@
 """Entry point to the API."""
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from exceptions import BaseCustomException
+from fastapi import FastAPI
+from exceptions import exception_handlers
 from routes import user, activity_levels, training_plan
 
 
-app = FastAPI()
-
+app = FastAPI(exception_handlers=exception_handlers)
 
 @app.get('')
 def ping() -> str:
     """Test endpoint."""
     return 'Hello, User'
-
-# include exception handlers here
-@app.exception_handler(BaseCustomException)
-async def custom_error_handler(request: Request,
-                               exc: BaseCustomException) -> JSONResponse:
-    """
-    Handle all custom exceptions.
-    
-    All defined exceptions in `exceptions.py` will be handled here.
-    """
-    return JSONResponse(
-        status_code=exc.status,
-        content={
-            "message": exc.message,
-            "type": exc.__class__.__name__,  
-            "detail": str(exc)
-            }
-        )
-
 
 # include routers here
 app.include_router(user.router)
